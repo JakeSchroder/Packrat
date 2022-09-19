ec1796c3681ae9ca8b5e6d9913d410aa4241456f
JakeSchroder <jake.schroder@gmail.com>
2022-09-18 23:02:41 -0500

    Rename variables for clarity

--- ui/src/containers/ProductGrid.js
+++ ui/src/containers/ProductGrid.js
@@ -1,13 +1,10 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
 import ItemComponent from "../components/Item";
 //TODO//
 //* Make map function to add all products *//
 
-export default function BasicGrid({ filterState, products }) {
+export default function BasicGrid({ productData }) {
   return (
     <Grid
       justifyContent="space-around"
@@ -16,7 +13,7 @@ export default function BasicGrid({ filterState, products }) {
       columnSpacing={{ xs: 1, sm: 2, md: 3 }}
       rowBuffer={200}
     >
-      {products.map((product, index) => ItemComponent({ ...product }, index))}
+      {productData.map((product, index) => ItemComponent({ ...product }, index))}
     </Grid>
   );
 }
--- ui/src/home/HomePage.js
+++ ui/src/home/HomePage.js
@@ -6,18 +6,18 @@ import ProductGridContainer from "../containers/ProductGrid";
 const HomePage = () => {  
   const [filterState, setFilterState] = useState("All");
   const [isLoading, setIsLoading] = useState(true);
-  const [products, setProducts] = useState([]);
+  const [productData, setProductData] = useState([]);
   const [error, setError] = useState(false);
   const [pageIndex, setPageIndex] = useState(0);
   const pageSize = 18;
 
-  const fetchData = useCallback(async() =>{
+  const fetchProductData = useCallback(async() =>{
     if(isLoading){
       try{
         // fetch request for products
         fetch(`http://localhost:3080/products/${filterState}?pageIndex=${pageIndex}&pageSize=${pageSize}`)
           .then((response) => response.json())
-          .then((data) => setProducts([...products, ...data]));
+          .then((data) => setProductData([...productData, ...data]));
         // After successful fetch set isLoading to false
         setIsLoading(false);
       }catch(err){
@@ -45,7 +45,7 @@ const HomePage = () => {
   };
 
   useEffect(() => {
-    fetchData();// Make API call for products here
+    fetchProductData();// Make API call for products here
     window.addEventListener('scroll', handleScroll);
 
     return () => {
@@ -61,7 +61,7 @@ const HomePage = () => {
     <div className="App">
       <header className="App-header">
         <PageHeaderContainer setFilterState={setFilterState} setIsLoading={setIsLoading} />
-        <ProductGridContainer products={products} />
+        <ProductGridContainer productData={productData} />
         <PageFooterContainer />
       </header>
     </div>
