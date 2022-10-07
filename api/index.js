dda37ccc619bc00ff3d1604a58120679318d6600
JakeSchroder <jake.schroder@gmail.com>
2022-10-07 02:51:49 -0500

    Add comments to index.js

--- api/index.js
+++ api/index.js
@@ -3,33 +3,35 @@ const bodyParser = require('body-parser');
 const {products} = require('./src/routes');
 const mongoose = require('mongoose');
 const path = require('path');
-const cors = require('cors');
 
+// Connect to MongoDB
 const mongoString = process.env.DATABASE_URL;
 mongoose.connect(mongoString);
-
 const database = mongoose.connection
+
 database.on('error', (error) => {
     console.log(error)
 })
-
 database.once('connected', () => {
     console.log('Database Connected');
 })
 
 const app = express();
+
+// Assign header value for CORS
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use(bodyParser.json());
 app.use(products);
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
 
+// Serve UI to the client
 if(process.env.ENVIRONMENT === 'production'){
   app.use(express.static(path.join(__dirname, 'build')));
-  app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  app.get('/', function (request, response) {
+    response.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 
